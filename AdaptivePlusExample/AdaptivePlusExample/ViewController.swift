e3879f2d287d8025e3b8cac7aa5382af7786a59f
AdaptivePlus <dev@adaptive.plus>
2021-05-26 20:35:52 +0600

    feat: update example app

--- AdaptivePlusExample/AdaptivePlusExample/ViewController.swift
+++ AdaptivePlusExample/AdaptivePlusExample/ViewController.swift
@@ -10,32 +10,64 @@ import AdaptivePlus
 
 class ViewController: UIViewController {
 
-    var apView: APView!
+//    var apView: APView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        setupApativeView()
+
+//        setupApativeView()
+        showSplashScreen()
     }
 
-    func setupApativeView() {
-        apView = AdaptivePlus.instance.createAPView()
-        view.addSubview(apView)
+//    func setupApativeView() {
+//        apView = AdaptivePlus.instance.createAPView()
+//        view.addSubview(apView)
+//
+//        NSLayoutConstraint.activate([
+//            apView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32),
+//            apView.leftAnchor.constraint(equalTo: view.leftAnchor),
+//            apView.rightAnchor.constraint(equalTo: view.rightAnchor)
+//        ])
+//
+//    }
 
-        NSLayoutConstraint.activate([
-            apView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32),
-            apView.leftAnchor.constraint(equalTo: view.leftAnchor),
-            apView.rightAnchor.constraint(equalTo: view.rightAnchor)
-        ])
+    func showSplashScreen() {
+        let user = AdaptivePlusUser(
+            // In app Client Identifier (Email/Phone/Internal user id)
+            userId: nil,
+            // Client location
+            userCoordinate: nil,
+            // In app Client Properties (Age/Gender/Country/VIP Status, etc)
+            properties: ["gender": "MALE", "age": "20"])
 
+        let customActionTriggered: ((String, [String : Any]) -> Void) = { name, parameters in
+            print("Custom action name: \(name)")
+            print("Custom action parameters: \(parameters)")
+        }
+
+        let splashScreenFinished: (() -> Void) = {
+            print("Splash screen finished")
+        }
+
+        AdaptivePlus.showSplashScreen(
+            user: user, // optional
+            hasDrafts: true, // optional, default value - false
+            customActionTriggered: customActionTriggered, // optional
+            finished: splashScreenFinished // optional
+        )
     }
 
     @IBAction func reloadButtonTouched(_ sender: Any) {
-        apView.reload()
+//        apView.reload()
     }
 
     @IBAction func scrollToStart(_ sender: Any) {
-        apView.scrollToStart()
+//        apView.scrollToStart()
+    }
+
+    @IBAction func showSplashScreenTouched(_ sender: Any) {
+        showSplashScreen()
     }
 
 }
